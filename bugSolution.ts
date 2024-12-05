c4e523b4bb52eec09a54e2d2ea5598705d534ea2
BugHunter-X <askzenai@gmail.com>
2024-12-05 12:24:05 +0100

    Add bugSolution.ts file

---
+++ bugSolution.ts
@@ -0,0 +1,11 @@
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+let result = add(1, 2); // Correct
+
+function addString(a: string, b: string): string{
+  return a + b; //String Concatenation
+}
+
+let resultString = addString("1","2"); //Correct
